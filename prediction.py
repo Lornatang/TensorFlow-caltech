fe2594a4c35fa6f3db9aea1a1933b677ee6031f0
Lornatang <shiyipaisizuo@163.com>
2019-05-30 14:32:16 +0800

    add auto detection

--- prediction.py
+++ prediction.py
@@ -17,15 +17,10 @@ from dataset import get_label_name
 
 import tensorflow as tf
 
-import matplotlib.pyplot as plt
-from PIL import Image
-
 import argparse
 
 parser = argparse.ArgumentParser('Prediction mnist label')
 
-parser.add_argument('--path', type=str,
-                    help='Image path, best input abs path. `./datasets/cat.png`')
 parser.add_argument('--height', type=int, default=224,
                     help='Image height. default: 224')
 parser.add_argument('--width', type=int, default=224,
@@ -36,12 +31,33 @@ parser.add_argument('--classes', type=int, default=102,
                     help="Classification picture type. default: 102")
 parser.add_argument('--checkpoint_dir', '--dir', type=str, default='training_checkpoint',
                     help="Model save path.")
-parser.add_argument('--dis', type=bool, default=False,
-                    help='display matplotlib? default: False.')
 args = parser.parse_args()
 
 label_names = get_label_name()
 label_names = label_names.features['label'].int2str
+
+base_model = MobileNetV2(include_top=False,
+                         input_shape=(args.height, args.width, args.channels),
+                         weights='imagenet',
+                         classes=args.classes)
+
+avg_pool = tf.keras.layers.GlobalAveragePooling2D()
+fc = tf.keras.layers.Dense(args.classes,
+                           activation=tf.nn.softmax,
+                           name='Logits')
+
+model = tf.keras.Sequential([
+  base_model,
+  avg_pool,
+  fc
+])
+
+print(f"==========================================")
+print(f"Loading model.............................")
+checkpoint = tf.train.Checkpoint(model=model)
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir=args.checkpoint_dir))
+print(f"Load model successful!")
+print(f"==========================================")
 
 
 def process_image(image, height=args.height, width=args.width):
@@ -66,62 +82,41 @@ def process_image(image, height=args.height, width=args.width):
   image = image / 255.
   # image resize model input size.
   image = tf.image.resize(image, (height, width))
+
   return image
 
 
-def prediction(image):
+def prediction(img):
   """ prediction image label.
 
   Args:
-    image: 'input tensor'.
+    img: 'input tensor'.
 
   Returns:
     'int64', label
 
   """
-  image = process_image(image)
+  image = process_image(img)
   # Add the image to a batch where it's the only member.
   image = (tf.expand_dims(image, 0))
 
-  base_model = MobileNetV2(include_top=False,
-                           input_shape=(args.height, args.width, args.channels),
-                           weights='imagenet',
-                           classes=args.classes)
-
-  avg_pool = tf.keras.layers.GlobalAveragePooling2D()
-  fc = tf.keras.layers.Dense(args.classes,
-                             activation=tf.nn.softmax,
-                             name='Logits')
-
-  model = tf.keras.Sequential([
-    base_model,
-    avg_pool,
-    fc
-  ])
-
-  print(f"==========================================")
-  print(f"Loading model.............................")
-  checkpoint = tf.train.Checkpoint(model=model)
-  checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir=args.checkpoint_dir))
-  print(f"Load model successful!")
-  print(f"==========================================")
   print(f"Start making predictions about the picture.")
-  print(f"==========================================")
 
   predictions = model(image)
   classes = int(tf.argmax(predictions[0]))
+  print(f"==========================================")
   print(f"Label is : {label_names(classes)}")
-
-  if args.dis:
-    image = Image.open(args.path)
-    plt.figure(figsize=(4, 4))
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(image, cmap='gray')
-    plt.xlabel(label_names(classes))
-    plt.show()
+  print(f"==========================================")
+  print()
 
 
 if __name__ == '__main__':
-  prediction(args.path)
+  while True:
+    a = input("files(input '0' exit.):")
+    if a == '0':
+      exit(0)
+      print("Successful exit. return status 0.")
+    else:
+      a = a[:-1]
+      a = tf.cast(a, tf.string)
+      prediction(a)
